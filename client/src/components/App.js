92767ea6c70853879e8176e2461691977aca21e0
nzbenji <benlittle53@gmail.com>
2019-08-25 11:16:38 +1200

    retrieving brand data from strapi cms

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { Container, Box, Heading } from 'gestalt';
+import Strapi from 'strapi-sdk-javascript/build/main';
+
+const apiURL = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiURL);
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await strapi.request('POST', '/graphql', {
+      data: {
+        query: `query {
+          brands {
+            _id
+            name
+            description
+            createdAt
+            image {
+              name
+            }
+          }
+        }`
+      }
+    });
+    console.log(res);
+  }
+
   render() {
     return (
       <Container>
-        <Box
-          display="flex"
-          justifyContent="center"
-          marginBottom={2}
-        >
-        <Heading
-        color="midnight" size="md">
-          Brew Brands
-        </Heading>
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
         </Box>
       </Container>
     );
