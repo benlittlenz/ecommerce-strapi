fe51900dfe3ccd24227bd2c4d7a5d0250a4e43d3
nzbenji <benlittle53@gmail.com>
2019-08-31 12:29:52 +1200

    sign in  implemented

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,8 +1,107 @@
 import React from 'react';
+import { Container, Box, Button, Heading, Text, TextField } from 'gestalt';
+import { setToken } from '../utils';
+import ToastMessage from './ToastMessage';
+import Strapi from 'strapi-sdk-javascript/build/main';
+
+const apiURL = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiURL);
 
 class Signin extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    toast: false,
+    toastMessage: '',
+    loading: false
+  };
+
+  handleChange = ({ event, value }) => {
+    event.persist();
+    this.setState({ [event.target.name]: value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { username, password } = this.state;
+
+    if (this.isFormEmpty(this.state)) {
+      this.showToast('Fill in all fields');
+      return;
+    }
+    try {
+      this.setState({ loading: true });
+      const res = await strapi.login(username, password);
+      
+      this.setState({ loading: false });
+      console.log(res);
+      setToken(res.jwt);
+      this.redirectUser('/');
+    } catch (err) {
+      this.setState({ loading: false });
+      this.showToast(err.message);
+    }
+  };
+
+  redirectUser = path => this.props.history.push(path);
+
+  isFormEmpty = ({ username, password }) => {
+    return !username || !password;
+  };
+
+  showToast = toastMessage => {
+    this.setState({ toast: true, toastMessage });
+    setTimeout(() => this.setState({ toast: false, toastMessage: '' }), 5000);
+  };
+
   render() {
-    return <div></div>;
+    const { toastMessage, toast, loading } = this.state;
+
+    return (
+      <Container>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#ebe2da'
+            }
+          }}
+          margin={4}
+          padding={4}
+          shape="rounded"
+          display="flex"
+          justifyContent="center"
+        >
+          <form
+            style={{
+              display: 'inlineBlock',
+              textAlign: 'center',
+              maxWidth: 450
+            }}
+            onSubmit={this.handleSubmit}
+          >
+            <Box marginBottom={2} display="flex" direction="column" alignItems="center">
+            </Box>
+            <TextField
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+            <Button inline disabled={loading} color="blue" text="Submit" type="submit" />
+          </form>
+        </Box>
+        <ToastMessage show={toast} message={toastMessage} />
+      </Container>
+    );
   }
 }
 
