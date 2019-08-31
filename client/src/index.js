0bc4579d31d9ec3d32d84e51e7dcf4efb9efa3eb
nzbenji <benlittle53@gmail.com>
2019-08-31 15:20:37 +1200

    made checkout a private page

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { getToken } from './utils';
 import 'gestalt/dist/gestalt.css';
 
 import Navbar from './components/Navbar';
@@ -10,6 +11,24 @@ import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import Brews from './components/Brews';
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      getToken() !== null ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/signin',
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
 const Root = () => (
   <Router>
     <React.Fragment>
@@ -18,7 +37,7 @@ const Root = () => (
         <Route component={App} exact path="/" />
         <Route component={Signin} path="/signin" />
         <Route component={Signup} path="/signup" />
-        <Route component={Checkout} path="/checkout" />
+        <PrivateRoute component={Checkout} path="/checkout" />
         <Route component={Brews} path="/:brandId" />
       </Switch>
     </React.Fragment>
